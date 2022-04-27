7719ba43129d6e35c4ecf15c55259b06064ff3a9
modest_ <1342994266@qq.com>
2022-04-27 22:39:31 +0800

    初始化-项目搭建

---
+++ src/App.js
@@ -0,0 +1,3 @@
+export default function App() {
+  return <div>app组件</div>
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom'
+import App from './App'
+// 导入通用样式
+import './assets/styles/index.scss'
+
+ReactDOM.render(<App />, document.getElementById('root'))
