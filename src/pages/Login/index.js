cf5ce4bdf9669c5e36fb9e1f0ed959b5c0b0037c
modest_ <1342994266@qq.com>
2022-04-28 21:13:26 +0800

    登录功能-表单验证

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,6 +2,8 @@ import NavBar from '@/components/NavBar'
 import styles from './index.module.scss'
 import Input from '@/components/Input'
 import { useFormik } from 'formik'
+import * as Yup from 'yup'
+import classNames from 'classnames'
 
 export default function Login() {
   const onExtraClick = () => {
@@ -15,12 +17,24 @@ export default function Login() {
     },
     onSubmit: values => {
       console.log(values)
-    }
+    },
+    validationSchema: Yup.object({
+      mobile: Yup.string()
+        .required('手机号不能为空')
+        .matches(/^1[3-9]\d{9}$/, '手机号格式错误'),
+      code: Yup.string()
+        .required('验证码不能为空')
+        .matches(/^\d{6}$/, '验证码格式错误')
+    })
   })
   const {
     values: { mobile, code },
     handleChange,
-    handleSubmit
+    handleSubmit,
+    handleBlur,
+    errors,
+    touched,
+    isValid
   } = formik
   return (
     <div className={styles.root}>
@@ -31,15 +45,15 @@ export default function Login() {
         <h3>短信登录</h3>
         <form onSubmit={handleSubmit}>
           <div className="input-item">
-            <Input placeholder="请输入手机号" autoComplete="off" name="mobile" value={mobile} onChange={handleChange}></Input>
-            {/* <div className="validate">手机号验证错误信息</div> */}
+            <Input placeholder="请输入手机号" autoComplete="off" name="mobile" value={mobile} onChange={handleChange} onBlur={handleBlur} maxLength={11}></Input>
+            {touched.mobile && errors.mobile ? <div className="validate">{errors.mobile}</div> : null}
           </div>
           <div className="input-item">
-            <Input placeholder="请输入验证码" autoComplete="off" name="code" extra="获取验证码" onExtraClick={onExtraClick} value={code} onChange={handleChange}></Input>
-            {/* <div className="validate">验证码验证错误信息</div> */}
+            <Input placeholder="请输入验证码" autoComplete="off" name="code" extra="获取验证码" onExtraClick={onExtraClick} value={code} onChange={handleChange} onBlur={handleBlur} maxLength={6}></Input>
+            {touched.code && errors.code ? <div className="validate">{errors.code}</div> : null}
           </div>
           {/* 登录按钮 */}
-          <button type="submit" className="login-btn">
+          <button type="submit" className={classNames('login-btn', { disabled: !isValid })} disabled={!isValid}>
             登录
           </button>
         </form>
